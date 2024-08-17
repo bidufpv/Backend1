b627fc97b54387829516c5afdfcd3f7cf195abee
bidufpv <bidufpv@gmail.com>
2024-08-17 20:40:27 +0530

    commit w

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log('helloworld');
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({path: './env'});
+connectDB();
+
+
+
+
+
+
+
+
+
+// // const DB_NAME = "myVideo";
+// import express from "express";
+// const app = express();
+// const port = process.env.PORT;
+
+// (async()=>{
+//     try {
+//         mongoose.connect(`${process.env.MONGO}/${DB_NAME}`)
+//         app.on("error", (error)=>{
+//             console.log("Error:", error);
+//             throw error;
+//         })
+
+//         app.listen(port, ()=>{
+//             console.log(`Server is listening on Port ${port}`);
+            
+//         })
+
+//     } catch (error) {
+//         console.log("Error:", error);
+//         throw error;
+//     }
+// })()
